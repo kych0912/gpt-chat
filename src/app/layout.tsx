9dd80a089332ec459b48b8be71cf2ec64cfe04f4
YoungCheon Kim <kyt031522@gmail.com>
2025-01-09 17:34:00 +0900

    feat: google analytics 설치

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Providers from './providers';
 import LayoutProvider from './provider/LayoutProvider';
 import { pretendard } from "./style/fonts/fonts";
+import Analytics from './components/GA/Analytics';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en" className={pretendard.variable}>
       <body className={inter.className}>
+        {
+          process.env.NEXT_PUBLIC_GA_ID && (
+            <Analytics id={process.env.NEXT_PUBLIC_GA_ID} />
+          )
+        }
         <LayoutProvider>
           <Providers>{children}</Providers>
         </LayoutProvider>
